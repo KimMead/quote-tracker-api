43b9ca2ded13befea88b322c98385e67c2ce4857
KimMead <kimmeadows8@gmail.com>
2020-01-27 19:52:29 -0600

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_234325) do
+ActiveRecord::Schema.define(version: 2020_01_28_012509) do
 
   create_table "quotes", force: :cascade do |t|
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
